c32900fe77e993b3594b5d0986c1026e334b5e26
Shivam15483 <35308764+Shivam15483@users.noreply.github.com>
2018-01-16 23:26:39 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+printf("enter a character");
+scanf("%c",&a);
+if(a=='A'||a=='a'||a=='E'||a=='e'||a=='I'||a=='i'||a=='O'||a=='o'||a=='U'||a=='u')
+printf("Vowel");
+else
+printf("Consonant");
+}
